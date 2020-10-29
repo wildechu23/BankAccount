110cc736bcf80f68251a26216b00e1e48858e9bb
Wilde Chu <wildechu23@gmail.com>
2020-10-29 12:06:41 -0400

    edited transferto

--- BankAccount.java
+++ BankAccount.java
@@ -44,13 +44,16 @@ public class BankAccount {
   }
 
   private boolean authenticate(String password) {
-    return password == this.password;
+    return this.password.equals(password);
   }
 
   public boolean transferTo(BankAccount other, double amount, String password) {
     if(authenticate(password) && this.withdraw(amount)) {
-      other.deposit(amount);
-      return true;
+      if(other.deposit(amount){
+        return true;
+      } else {
+        return false; // break
+      }
     }
     return false;
   }
